0d1b92c24ea0d2c36dd3c448d9a0fda7599c66c9
Akkshay Khoslaa <akkshay@Akkshays-MacBook-Pro.local>
2015-01-02 09:43:30 -0400

    Initial Commit

---
+++ InstClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstClone
+//
+//  Created by Akkshay Khoslaa on 1/2/15.
+//  Copyright (c) 2015 Akkshay Khoslaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
